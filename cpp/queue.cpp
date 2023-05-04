5cd06a90cea008e14b3a1d79dd9d989e78effb2b
pegasis470 <101937683+pegasis470@users.noreply.github.com>
2023-05-05 01:44:37 +0530

    Update queue.cpp

--- cpp/queue.cpp
+++ cpp/queue.cpp
@@ -3,90 +3,87 @@
 
 using namespace std;
 
-class Queue{
-	int queue[Max_size];
-	int front=-1;
-	int rear=-1;
-	public:
-		int Enqueue(){
-			int ele,before;
-			if (front>=Max_size){
-				cout<<"OverflowError: Queue is full\n\n";
-				return 1;
-			}
-			else {
-				cout<<"enter element to enqueue:\n";
-				cin>>ele;
-				if (front==-1 && rear==-1)
-					rear++;
-				else
-					;
-				before=queue[front];
-				front++;
-				queue[front]=before;
-				queue[rear]=ele;
-				cout<<ele<<" Added to queue\n";
-				return 0;
-			}
-		}
-		int Display(){
-			if (front==-1){
-				cout<<"Queue is empty\n";
-				return 1;
-			}
-			else{
-				cout<<"The elements of the queue are\n";
-				for(int i=1;i<=front;i=i+1)
-					cout<<queue[i]<<"  ";
-				return 0;
-			}
-		}
+class Linear_Queue{
+        int queue[Max_size];
+        int front=-1;
+        int rear=-1;
+        public:
+                int Enqueue(){
+                        int ele;
+                        if (rear == Max_size-1){
+                                cout<<"OverflowError: Queue is full\n\n";
+                                return 1;
+                        }
+                        else {
+                                cout<<"enter element to enqueue:\n";
+                                cin>>ele;
+                                if (front==-1 && rear==-1)
+                                        front++;
+                                else
+                                        ;
+                                rear++;
+                                queue[rear]=ele;
+                                cout<<ele<<" Added to queue\n";
+                                return 0;
+                        }
+                }
+                int Display(){
+                        if (front==-1){
+                                cout<<"Queue is empty\n";
+                                return 1;
+                        }
+                        else{
+                                cout<<"The elements of the queue are\n";
+                                for(int i = front; i <=rear; i++){
+                                        cout<<queue[i]<<" ";
+                                }
+                                return 0;
+                        }
+                }
 
-		int Dequeue(){
-			if (front==-1){
-				cout<<"UnderFlowerror: Queue is empty\n";
-				return 1;
-			}
-			else{
-				cout<<queue[front]<<" Removed from queue\n";
-				front--;
-				return 0;
-			}
-		}
-};
+                int Dequeue(){
+                        if (front>rear){
+                                cout<<"UnderFlowerror: Queue is empty\n";
+                                return 1;
+                        }
+                        else{
+                                cout<<queue[front]<<" Removed from queue\n";
+                                front++;
+                                return 0;
+                        }
+                }
+	};
 
 
 int main(){
-	Queue Q;
-	int choice;
-	int loop=3;
-	while(loop==3){
-		cout<<"\npress 1 to enqueue \npress 2 to dequeue\npress 3 to display\n";
-		cin>>choice;
-		if (choice==1){
-			if(Q.Enqueue())
-				break;
-			else
-				choice=0;
-		}
-		else if (choice==2){
-			if (Q.Dequeue())
-				break;
-			else
-				choice;
-		}
-		else if(choice == 3) {
-			if (Q.Display())
-				break;
-			else
-				choice;
-		}
-		else{
+        Linear_Queue Q;
+        int choice;
+        int loop=3;
+        while(loop==3){
+                cout<<"\npress 1 to enqueue \npress 2 to dequeue\npress 3 to display\n";
+                cin>>choice;
+                if (choice==1){
+                        if(Q.Enqueue())
+                                break;
+                        else
+                                choice=0;
+                }
+                else if (choice==2){
+                        if (Q.Dequeue())
+                                break;
+                        else
+                                choice;
+                }
+                else if(choice == 3) {
+                        if (Q.Display())
+                                break;
+                        else
+                                choice;
+                }
+                else{
 
-			cout<<"Invalid choice exiting";
-			break;
-		}
-	}
+                        cout<<"Invalid choice exiting";
+                        break;
+                }
+        }
 }
-
-
